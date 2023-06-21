482bc611754f29d92c25a7e6f588994f7d9afd4e
NicolasLDRFR <nicolaslaudier@gmail.com>
2023-06-21 16:16:52 +0200

    plot_f1_score CNN

--- Notebooks/3_CNN/Fonctions_utils.py
+++ Notebooks/3_CNN/Fonctions_utils.py
@@ -2,6 +2,8 @@ from tensorflow.keras import backend as K
 import seaborn as sns
 import numpy as np
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
 def recall_m(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
@@ -23,3 +25,31 @@ def f1_m(y_true, y_pred):
 def matrix_confusion(y_test, y_pred):
     cf_matrix = confusion_matrix(y_test, y_pred)
     sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Blues')
+    
+def plot_score_model(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    f1_m = history.history['f1_m']
+    val_f1_m = history.history['val_f1_m']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 3, 1)
+    plt.plot(x, f1_m, 'b', label='Training f1 Score')
+    plt.plot(x, val_f1_m, 'r', label='Validation f1 Score')
+    plt.title('F1 score')
+    plt.legend()
+    
+    plt.subplot(1, 3, 2)    
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Accuracy')
+    plt.legend()
+    
+    plt.subplot(1, 3, 3)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Loss')
+    plt.legend()
